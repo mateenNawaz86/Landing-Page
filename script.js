a73f694e3c697b18eb0c8ba10542f7685382a4b2
Mateen Mirani <mateenmirani125@gmail.com>
2021-06-18 08:47:10 +0500

    add some files

---
+++ script.js
@@ -0,0 +1,15 @@
+function facebookRendering() {
+    window.location.href = 'https://www.facebook.com/mateennawaz.mateenawaz/"';
+}
+
+function twitterRendering() {
+    window.location.href = 'https://twitter.com/MateenMirani2';
+}
+
+function instaRendering() {
+    window.location.href = 'https://www.instagram.com/merani_66/';
+}
+
+function youtubeRendering() {
+    window.location.href = 'https://www.youtube.com/';
+}
